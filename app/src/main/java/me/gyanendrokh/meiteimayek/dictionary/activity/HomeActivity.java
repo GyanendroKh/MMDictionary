6fd5b3c94a4c19e9b3f7560e3290b4213e230cd6
Gyanendro Kh <khgyanendro77@gmail.com>
2018-07-15 08:29:47 +0530

    Added the HomeActivity

---
+++ app/src/main/java/me/gyanendrokh/meiteimayek/dictionary/activity/HomeActivity.java
@@ -0,0 +1,106 @@
+package me.gyanendrokh.meiteimayek.dictionary.activity;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.lapism.searchview.Search;
+import com.lapism.searchview.widget.SearchView;
+
+import me.gyanendrokh.meiteimayek.dictionary.R;
+import me.gyanendrokh.meiteimayek.dictionary.adapter.HomePagerAdapter;
+
+public class HomeActivity extends AppCompatActivity
+  implements NavigationView.OnNavigationItemSelectedListener {
+
+  private DrawerLayout mDrawer;
+  private SearchView mSearchView;
+
+  private long mLastBackPressed = 0;
+
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_home);
+
+    initView();
+  }
+
+  private void initView() {
+    this.mDrawer = findViewById(R.id.drawer_layout);
+
+    initSearchView();
+    initPager();
+
+    ((NavigationView)findViewById(R.id.nav_view))
+      .setNavigationItemSelectedListener(this);
+  }
+
+  private void initSearchView() {
+    this.mSearchView = findViewById(R.id.search_view);
+    this.mSearchView.setOnLogoClickListener(() -> mDrawer.openDrawer(GravityCompat.START));
+
+    this.mSearchView.setOnQueryTextListener(new Search.OnQueryTextListener() {
+      @Override
+      public boolean onQueryTextSubmit(CharSequence query) {
+        mSearchView.setQuery("", false);
+        mSearchView.close();
+
+        return true;
+      }
+
+      @Override
+      public void onQueryTextChange(CharSequence newText) {
+
+      }
+    });
+  }
+
+  private void initPager() {
+    ViewPager viewPager = findViewById(R.id.home_view_pager);
+    TabLayout tabLayout = findViewById(R.id.home_tab_layout);
+
+    viewPager.setAdapter(new HomePagerAdapter(getSupportFragmentManager()));
+    viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+    tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(viewPager));
+  }
+
+  @Override
+  public void onBackPressed() {
+    if (mDrawer.isDrawerOpen(GravityCompat.START))
+      mDrawer.closeDrawer(GravityCompat.START);
+    else {
+      if(mLastBackPressed == 0) {
+        mLastBackPressed = System.currentTimeMillis();
+        Toast.makeText(this, "Press again to exit.", Toast.LENGTH_SHORT).show();
+      }else {
+        if((System.currentTimeMillis() - mLastBackPressed) < 3000) {
+          super.onBackPressed();
+        }else {
+          mLastBackPressed = 0;
+          onBackPressed();
+        }
+      }
+    }
+  }
+
+  @Override
+  public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+    mDrawer.closeDrawer(GravityCompat.START);
+
+    switch(item.getItemId()) {
+      case R.id.nav_about:
+        return false;
+    }
+
+    return true;
+  }
+}
---
+++ app/src/main/java/me/gyanendrokh/meiteimayek/dictionary/adapter/HomePagerAdapter.java
@@ -0,0 +1,31 @@
+package me.gyanendrokh.meiteimayek.dictionary.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class HomePagerAdapter extends FragmentPagerAdapter{
+
+  public HomePagerAdapter(FragmentManager fm) {
+    super(fm);
+  }
+
+  @Override
+  public Fragment getItem(int position) {
+    return Factory.FRAGMENTS[position];
+  }
+
+  @Override
+  public int getCount() {
+    return Factory.FRAGMENTS.length;
+  }
+
+  private static class Factory {
+
+    private static Fragment FRAGMENTS[] = {
+
+    };
+
+  }
+
+}
